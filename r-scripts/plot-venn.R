26fcfc960318efbd6c6020f3b2a755add454a77f
Sorel Fitz-Gibbon <sfitzgibbon@mednet.ucla.edu>
2023-08-02 17:24:40 -0700

    use file.path() and update changelog

--- r-scripts/plot-venn.R
+++ r-scripts/plot-venn.R
@@ -31,11 +31,11 @@ plot.venn <- function(tool.variants, outfile) {
 
 ### Main ###########################################################################################
 # Get intersection counts from BCFtools isec output and format for plotting
-algorithms <- readLines(paste0(args$isec_dir,'/README.txt'));
+algorithms <- readLines(file.path(args$isec_dir,'README.txt'));
 algorithms <- algorithms[grep(paste0('^', args$isec_dir), algorithms)];
 algorithms <- gsub(paste0(args$isec_dir,'.*\t'), '', algorithms);
 algorithms <- gsub('-.*', '', algorithms);
-sites <- read.table(paste0(args$isec_dir,'/sites.txt'), header = FALSE, colClasses = 'character');
+sites <- read.table(file.path(args$isec_dir,'sites.txt'), header = FALSE, colClasses = 'character');
 split.col <- strsplit(as.character(sites$V5), '');
 sites$col1 <- sapply(split.col, '[', 1);
 sites$col2 <- sapply(split.col, '[', 2);
@@ -45,6 +45,6 @@ sites$V5 <- NULL;
 header <- c('chrom', 'pos', 'ref', 'alt', algorithms);
 colnames(sites) <- header
 variants <- paste(sites$chrom, sites$pos, sep = '_');
-tool.variants <- lapply(sites[, algorithms], function(x) variants[x == 1])
+tool.variants <- lapply(sites[, algorithms], function(x) variants[x == 1]);
 tool.variants.ordered <- tool.variants[order(lengths(tool.variants), decreasing = TRUE)];
 plot.venn(tool.variants.ordered, args$outfile);
